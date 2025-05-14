2d0a3d140b3b55bdc7f73594ef1f20fdb03be443
RanylFoumbi <ranylfoumbi@gmail.com>
2025-05-14 15:36:24 +0200

    init func

--- contact/contact.go
+++ contact/contact.go
@@ -5,3 +5,19 @@ type Contact struct {
 	LastName  string
 	Phone     string
 }
+
+type Annuaire struct {
+	Contacts []Contact
+}
+
+func (a Annuaire) AddContact(c Contact) {}
+
+func (a Annuaire) DeleteContact(c Contact) {}
+
+func (a Annuaire) FindContact(c Contact) {}
+
+func (a Annuaire) UpdateContact(c Contact) {}
+
+func (a Annuaire) GetAllContact(c Contact) {}
+
+func (a Annuaire) CheckContact(c Contact) {}
