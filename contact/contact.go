304b6108628e0b4a30ff6a536443666a061dac3a
rmalpar <robin.malpart@universite-paris-saclay.fr>
2025-05-14 15:48:43 +0200

    Change signature method for annuaire

--- contact/contact.go
+++ contact/contact.go
@@ -10,14 +10,14 @@ type Annuaire struct {
 	Contacts []Contact
 }
 
-func (a Annuaire) AddContact(c Contact) {}
+func (a *Annuaire) AddContact(c Contact) {}
 
-func (a Annuaire) DeleteContact(c Contact) {}
+func (a *Annuaire) DeleteContact(c Contact) {}
 
-func (a Annuaire) FindContact(c Contact) {}
+func (a *Annuaire) FindContact(c Contact) {}
 
-func (a Annuaire) UpdateContact(c Contact) {}
+func (a *Annuaire) UpdateContact(c Contact) {}
 
-func (a Annuaire) GetAllContact(c Contact) {}
+func (a *Annuaire) GetAllContact(c Contact) {}
 
-func (a Annuaire) CheckContact(c Contact) {}
+func (a *Annuaire) CheckContact(c Contact) {}
